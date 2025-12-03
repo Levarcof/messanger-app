dffa2066ce5feb5bc31b04233b5de2057e287d6c
Vikram <vikramp.1619@gmail.com>
2025-12-03 17:11:16 +0530

    fix

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -1,5 +1,6 @@
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/lib/auth";
+// app/actions/getSession.ts
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth"; // lib se import karo
 
 export default async function getSession() {
   return await getServerSession(authOptions);
